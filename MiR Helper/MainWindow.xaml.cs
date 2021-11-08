7d707a477c4731d9dba4eb7366c2c1abff52cd61
Tamas-N <89386535+Tamas-N@users.noreply.github.com>
2021-11-08 10:38:44 +0000

    Half ready: hard coded IP address and folder path; no error recovery

--- MiR Helper/MainWindow.xaml.cs
+++ MiR Helper/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MiR_Helper.rest;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -22,32 +23,158 @@ namespace MiR_Helper
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
+        private Robot robot;
+     
+
+      
+
+        public string StatusId
+        {
+            get { return (string)GetValue(dependencyStatusId); }
+            set { SetValue(dependencyStatusId, value); }
+        }
+
+        public string PosX
+        {
+            get { return (string)GetValue(dependencyPosX); }
+            set { SetValue(dependencyPosX, value); }
+        }
+
+        public string PosY
+        {
+            get { return (string)GetValue(dependencyPosY); }
+            set { SetValue(dependencyPosY, value); }
+        }
+
+        public string Orient
+        {
+            get { return (string)GetValue(dependencyOrient); }
+            set { SetValue(dependencyOrient, value); }
+        }
+
+        public string VelocityL
+        {
+            get { return (string)GetValue(dependencyVelocityL); }
+            set { SetValue(dependencyVelocityL, value); }
+        }
+
+        public string VelocityA
+        {
+            get { return (string)GetValue(dependencyVelocityA); }
+            set { SetValue(dependencyVelocityA, value); }
+        }
+
+        public static readonly DependencyProperty dependencyStatusId =
+            DependencyProperty.Register("StatusId", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+
+
+        public static readonly DependencyProperty dependencyPosX =
+            DependencyProperty.Register("PosX", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+
+
+        public static readonly DependencyProperty dependencyPosY =
+            DependencyProperty.Register("PosY", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+
+
+        public static readonly DependencyProperty dependencyOrient =
+            DependencyProperty.Register("Orient", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+
+
+        public static readonly DependencyProperty dependencyVelocityL =
+            DependencyProperty.Register("VelocityL", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+
+        
+        public static readonly DependencyProperty dependencyVelocityA =
+            DependencyProperty.Register("VelocityA", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            robot = new Robot();
+            robot.IpAddress = "192.168.17.174";
+            UpdateWindowValues();
+            stopTest.IsEnabled = false;
+
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private async void UpdateWindowValues()
         {
-            // Make a new data source object
-            robot currentRobot = new robot();
+            await robot.UpdateStatus();
 
-            // New binding object using the path of 'Name' for whatever source object is used
-            var ipBindingObject = new Binding("IP");
-
-            // Configure the binding
-            ipBindingObject.Mode = BindingMode.TwoWay;
-            ipBindingObject.Source = currentRobot;
-            ipBindingObject.Converter = IPConverter.Instance;
-            ipBindingObject.ConverterCulture = new CultureInfo("en-US");
-
-            // Set the binding to a target object. The TextBlock.Name property on the NameBlock UI element
-            BindingOperations.SetBinding(RobotIP, TextBlock.TextProperty, ipBindingObject);
+            this.StatusId = robot.Status.state_id.ToString();
+            this.PosX = robot.Status.position.x.ToString("0.##");
+            this.PosY = robot.Status.position.y.ToString("0.##");
+            this.Orient = robot.Status.position.orientation.ToString("0.##");
+            this.VelocityL = robot.Status.velocity.linear.ToString("0.##");
+            this.VelocityA = robot.Status.velocity.angular.ToString("0.##");
         }
+
+        //private void Window_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    // Make a new data source object
+        //    Robot currentRobot = new Robot();
+
+           
+        //    // New binding object using the path of 'Name' for whatever source object is used
+        //    //    var ipBindingObject = new Binding("IP");
+
+        //    //    // Configure the binding
+        //    //    ipBindingObject.Mode = BindingMode.TwoWay;
+        //    //    ipBindingObject.Source = currentRobot;
+        //    //    ipBindingObject.Converter = IPConverter.Instance;
+        //    //    ipBindingObject.ConverterCulture = new CultureInfo("en-US");
+
+
+
+        //    //    // Set the binding to a target object. The TextBlock.Name property on the NameBlock UI element
+        //    //    BindingOperations.SetBinding(RobotIP, TextBlock.TextProperty, ipBindingObject);
+
+        //}
 
         private void ButtonAddName_Click(object sender, RoutedEventArgs e)
         {
 
         }
+
+        private async void updateStatus_Click(object sender, RoutedEventArgs e)
+        {
+            await robot.UpdateStatus();
+            UpdateWindowValues();
+        }
+
+        private async void startTest_Click(object sender, RoutedEventArgs e)
+        {
+            stopTest.IsEnabled = true;
+            startTest.IsEnabled = false;
+            robot.UpdateTestStatus += OnUpdateTestStatus;
+            var testData = await robot.StoppingDistanceTest();
+            robot.SaveDataToFile(testData);
+            startTest.IsEnabled = true;
+            stopTest.IsEnabled = false;
+        }
+
+        private void stopTest_Click(object sender, RoutedEventArgs e)
+        {
+            startTest.IsEnabled = true;
+            stopTest.IsEnabled = false;
+            robot.StopTest = true;
+        }
+
+        public void OnUpdateTestStatus(object source, MirTestEventArgs args)
+        {
+            this.StatusId = args.TestData.MiRStatus.state_id.ToString();
+            this.PosX = args.TestData.MiRStatus.position.x.ToString("0.##");
+            this.PosY = args.TestData.MiRStatus.position.y.ToString("0.##");
+            this.Orient = args.TestData.MiRStatus.position.orientation.ToString("0.##");
+            this.VelocityL = args.TestData.MiRStatus.velocity.linear.ToString("0.##");
+            this.VelocityA = args.TestData.MiRStatus.velocity.angular.ToString("0.##");
+        }
+
     }
 }
--- MiR Helper/rest/robot.cs
+++ MiR Helper/rest/robot.cs
@@ -1,9 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.IO;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.Util;
+
+
 
 namespace MiR_Helper.rest
 {
@@ -11,20 +21,258 @@ namespace MiR_Helper.rest
     /// Out robot class will contain all of the data conatined within a robot. 
     /// This includes connection parameters (such as IP), along with other abstract data such as position, etc. 
     /// </summary>
-    public class robot
+    public class Robot
     {
         private static bool isFleet;
         private static IPAddress IP;
         private static AuthenticationManager Auth;
+        public string Authorization { get; set; }
+        public string IpAddress { get; set; }
+        public MiRStatus Status { get; private set; }
+        public bool StopTest { get; set; }
+
+        public event EventHandler<MirTestEventArgs> UpdateTestStatus;
 
         /// <summary>
         /// Empty constructor, just initialize all the variables so we don't get null exceptions later on
         /// </summary>
-        public robot()
+        public Robot()
         {
             isFleet = false;
             IP = new IPAddress(0x2414188f);
             /*Auth = new AuthenticationManager();*/
+        }
+
+        public async Task UpdateStatus()
+        {
+            Status = await GetMiRStatus();
+        }
+
+        public async Task<List<MiRTestData>> StoppingDistanceTest()
+        {
+            var testData = new List<MiRTestData>();
+            StopTest = false;
+
+
+            await UpdateStatus();
+            while(Status.state_id!= MiRStateId.Executing && Status.state_id!=MiRStateId.InManual)
+            {
+                if (StopTest)
+                    break;
+                OnUpdateTestStatus("Waiting for Executing or InManual State", new MiRTestData() { MiRStatus = Status, dataTime = TimeSpan.Zero });
+                Thread.Sleep(500);
+                await UpdateStatus();
+
+            }
+
+            var startime = DateTime.Now;
+            while(!(Status.state_id==MiRStateId.InError && Status.velocity.linear==0))
+            {
+                if (StopTest)
+                    break;
+                MiRTestData data = new MiRTestData();
+                await UpdateStatus();
+                var testTime = DateTime.Now-startime;
+                data.MiRStatus = Status;
+                data.dataTime = testTime;
+                testData.Add(data);
+                if(Status.state_id!=MiRStateId.InError)
+                    OnUpdateTestStatus("Test started, waiting for InError state", data);
+                else
+                    OnUpdateTestStatus("Robot in error, waiting for standstill state", data);
+                 Thread.Sleep(100);
+            }
+            OnUpdateTestStatus("Test finished", testData.Last());
+            return testData;
+        }
+
+        /// <summary>
+        /// Save the measurement data into an excel file
+        /// </summary>
+        public void SaveDataToFile(List<MiRTestData> TestData)
+        {
+
+            try
+            {
+                if (TestData == null)
+                    throw new Exception();
+
+                IWorkbook workbook = new XSSFWorkbook();
+                ISheet sheet1 = workbook.CreateSheet("MirTest");
+
+                // Creating header style
+                var boldFont = workbook.CreateFont();
+                boldFont.IsBold = true;
+                var headerStyle = workbook.CreateCellStyle();
+                headerStyle.SetFont(boldFont);
+                headerStyle.IsLocked = true;
+                headerStyle.BorderBottom = BorderStyle.Medium;
+                headerStyle.BorderLeft= BorderStyle.Medium;
+                headerStyle.BorderRight= BorderStyle.Medium; 
+                headerStyle.BorderTop= BorderStyle.Medium;
+                headerStyle.FillForegroundColor = IndexedColors.Yellow.Index;
+                headerStyle.FillPattern = FillPattern.SolidForeground;
+
+                // Filling header
+                IRow header = sheet1.CreateRow(0);
+                header.CreateCell(0).SetCellValue("Status");
+                header.CreateCell(1).SetCellValue("Position X");
+                header.CreateCell(2).SetCellValue("Position Y");
+                header.CreateCell(3).SetCellValue("Orientation");
+                header.CreateCell(4).SetCellValue("Linear Velocity");
+                header.CreateCell(5).SetCellValue("Angular Velocity");
+                header.CreateCell(6).SetCellValue("Time");
+                
+
+                for(int j=0;j<=6;j++)
+                {
+                    header.GetCell(j).CellStyle = headerStyle;
+                    sheet1.AutoSizeColumn(j);
+                    
+                    
+                }
+
+                // Creating formating for numeric values
+                var numStyle = workbook.CreateCellStyle();
+                numStyle.DataFormat = workbook.CreateDataFormat().GetFormat("0.00");
+
+
+                // Setting cell values
+                var i = 1;
+                foreach (var data in TestData)
+                {
+                    var row = sheet1.CreateRow(i);
+                    row.CreateCell(0).SetCellValue(data.MiRStatus.state_id.ToString());
+                    row.CreateCell(1).SetCellValue(data.MiRStatus.position.x);
+                    row.CreateCell(2).SetCellValue(data.MiRStatus.position.x);
+                    row.CreateCell(3).SetCellValue(data.MiRStatus.position.orientation);
+                    row.CreateCell(4).SetCellValue(data.MiRStatus.velocity.linear);
+                    row.CreateCell(5).SetCellValue(data.MiRStatus.velocity.angular);
+                    row.CreateCell(6).SetCellValue(data.dataTime.TotalSeconds);
+
+                    row.GetCell(1).CellStyle = numStyle;
+                    row.GetCell(2).CellStyle = numStyle;
+                    row.GetCell(3).CellStyle = numStyle;
+                    row.GetCell(4).CellStyle = numStyle;
+                    row.GetCell(5).CellStyle = numStyle;
+                    row.GetCell(6).CellStyle = numStyle;
+
+
+                    i++;
+                }
+
+                sheet1.CreateFreezePane(0, 1, 0, 1);
+                sheet1.AutoSizeColumn(0);
+
+                // Creating time-speed linechart
+                var numberOfRows = TestData.Count;
+                var drawing = sheet1.CreateDrawingPatriarch();
+                var anchor = drawing.CreateAnchor(0, 0, 0, 0, 8, 5, 18, 15);
+                var chart = drawing.CreateChart(anchor);
+                var legend = chart.GetOrCreateLegend();
+
+                var dataFactory = chart.ChartDataFactory;
+                var chartAxisFactory = chart.ChartAxisFactory;
+
+                var lineChartData = dataFactory.CreateLineChartData<double, double>();
+                var bottomAxis = chartAxisFactory.CreateCategoryAxis(NPOI.SS.UserModel.Charts.AxisPosition.Bottom);
+                var leftAxis = chartAxisFactory.CreateValueAxis(NPOI.SS.UserModel.Charts.AxisPosition.Right);
+                
+
+
+                var xs = NPOI.SS.UserModel.Charts.DataSources.FromNumericCellRange(sheet1, new NPOI.SS.Util.CellRangeAddress(0, numberOfRows, 6, 6));
+                var ys = NPOI.SS.UserModel.Charts.DataSources.FromNumericCellRange(sheet1, new NPOI.SS.Util.CellRangeAddress(0, numberOfRows, 4, 4));
+
+                lineChartData.AddSeries(xs, ys);
+                
+
+                chart.Plot(lineChartData, bottomAxis, leftAxis);
+
+                using (FileStream sw = File.Create(@"C:\Users\nemetht\MirTest\test.xlsx"))
+                {
+                    workbook.Write(sw);
+                    sw.Close();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+           
+            }
+            
+            
+        }
+
+        public async Task<MiRStatus> GetMiRStatus()
+        {
+            try
+            {
+                MiRStatus mirStatus = new MiRStatus();
+                var jBody = new JObject();
+                var urlBody = "status?whitelist=";
+
+
+
+                foreach (var whitelist in MiRStatus.WhiteList)
+                {
+                    urlBody += whitelist;
+                    if (MiRStatus.WhiteList.Last() != whitelist)
+                        urlBody += ",";
+                }
+
+                var rawStatus = await SendApiMessage(Method.GET, jBody, urlBody);
+
+                if (!rawStatus.IsSuccessful)
+                    throw new Exception("");
+
+                mirStatus = JsonConvert.DeserializeObject<MiRStatus>(rawStatus.Content);
+
+                return mirStatus;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private async Task<IRestResponse> SendApiMessage(Method method, JObject jObjectBody, string URLBody)
+        {
+
+            try
+            {
+                var baseURL = "http://" + IpAddress + "/api/v2.0.0/" + URLBody;
+
+
+                //restClient = new RestClient("http://192.168.1.216/api/v2.0.0/status?whitelist=state_id,position,velocity");
+                var restClient = new RestClient(baseURL);
+
+                restClient.Timeout = -1;
+
+                var request = new RestRequest(method);
+                request.AddHeader("Content-Type", "application/json");
+                request.AddHeader("Authorization", "Basic QWRtaW46OGM2OTc2ZTViNTQxMDQxNWJkZTkwOGJkNGRlZTE1ZGZiMTY3YTljODczZmM0YmI4YTgxZjZmMmFiNDQ4YTkxOA==");
+                request.AddHeader("Accept-Language", "en_US");
+
+
+                request.AddParameter("application/json", ParameterType.RequestBody);
+
+                IRestResponse response = await restClient.ExecuteAsync(request);
+
+
+                return response;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
         }
 
         public void GetIP(string RawIPAddress)
@@ -46,5 +294,65 @@ namespace MiR_Helper.rest
 
             }
         }
+
+        protected virtual void OnUpdateTestStatus(string testStatus, MiRTestData testData)
+        {
+            if (UpdateTestStatus != null)
+                UpdateTestStatus(this, new MirTestEventArgs() { TestStatus = testStatus, TestData = testData });
+        }
     }
+
+    public enum MiRStateId
+    {
+        ShuttingDown = 2,
+        Ready = 3,
+        Pause = 4,
+        Executing = 5,
+        Aborted = 6,
+        Completed = 7,
+        Docked = 8,
+        Docking = 9,
+        Estopped = 10,
+        InManual = 11,
+        InError = 12
+    }
+
+    public class MiRVelocity
+    {
+        public double linear { get; set; }
+        public double angular { get; set; }
+    }
+
+    public class MiRPosition
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public double orientation { get; set; }
+
+    }
+
+
+
+    public class MiRStatus
+    {
+        public MiRStateId state_id { get; set; }
+        public MiRVelocity velocity { get; set; }
+        public MiRPosition position { get; set; }
+
+        public static readonly string[] WhiteList = { "state_id", "velocity", "position" };
+    }
+
+    public class MiRTestData
+    {
+        public MiRStatus MiRStatus { get; set; }
+        public TimeSpan dataTime { get; set; }
+    }
+
+    public class MirTestEventArgs : EventArgs
+    {
+        public string TestStatus { get; set; }
+        public MiRTestData TestData { get; set; }
+    }
+
+
 }
